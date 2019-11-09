85f584c65c565891c934949ce78eaabe41503aea
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-11-09 12:51:52 +0530

    Putting few lines on implementation.

---
+++ Selection.py
@@ -0,0 +1,5 @@
+#in selection sort we basically select the smallest element from the array and then compare it with the first index of the list.
+#if the element is greater than the smallest element selected, then the element is swapped with that element.
+
+#Code
+
