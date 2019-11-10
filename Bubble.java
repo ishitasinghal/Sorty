d01e98953d1ec914d4803343982f1f29d2a70763
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-11-10 20:22:24 +0530

    Committing basic idea of the sorting technique.

---
+++ Bubble.java
@@ -0,0 +1,6 @@
+// In this type of sorting, swapping happens after comparing elements starting from index 0.
+// The first two elements are compared if first one is greater than the second one, then swap the numbers.
+// Likewise, the heaviest or the largest element goe to the end of the array.
+
+//CODE
+
