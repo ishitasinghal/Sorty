cc063d393d4f4889fbd932ce7be1b29135dad916
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-11-10 20:37:40 +0530

    Update Bubble.java

--- Bubble.java
+++ Bubble.java
@@ -4,3 +4,32 @@
 
 //CODE
 
+class Bubble
+{
+  void bsort(int arr[])
+  {
+    for(int i=0;i<n'i++)
+        {
+          for(int j=0;j<arr.length-i=1;j++) //to reduce the number of steps
+          {
+            if(arr[j]>arr[j+1])
+            {
+              int t = arr[j];
+              arr[j] = arr[j+1];
+              arr[j+1] = t;
+            }
+          }
+        }
+        }
+        
+        void print(int arr[])
+        {
+          for(int i = 0; i<arr.length;i++)
+          {
+            System.out.println(arr[i]+" ");
+          }
+          System.out.println();
+        }
+        
+        
+        
