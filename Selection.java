894f53f6e922cae312aa4f873d7fab9bd2e76630
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-11-11 15:01:46 +0530

    Update Selection.java

--- Selection.java
+++ Selection.java
@@ -1,7 +1,7 @@
-#in selection sort we basically select the smallest element from the array and then compare it with the first index of the list.
-#if the element is greater than the smallest element selected, then the element is swapped with that element.
+//in selection sort we basically select the smallest element from the array and then compare it with the first index of the list.
+//if the element is greater than the smallest element selected, then the element is swapped with that element.
 
-#Code
+//Code
 
 public class Selection
 {
