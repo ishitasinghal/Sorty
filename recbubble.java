31d4a85e37cb571df45d09c26cc6d91b3c8fa458
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-11-11 14:45:21 +0530

    Committing sort function !

--- recbubble.java
+++ recbubble.java
@@ -2,4 +2,22 @@
 //Same function is called recursively.
 
 //Code
-
+public class Bubble
+{
+  static void sortb(int arr[], int n) //n basically depicts the length of array.
+  {
+    if(n==1)
+      return;
+    
+    for(int i=0; i<n-1;i++)
+    {
+      if(arr[i]>arr[i+1])
+      {
+        int t = arr[i];
+        arr[i]=arr[i+1];
+        arr[i+1]=t;
+      }
+    }
+    sortb(arr, n-1);
+  }
+  
