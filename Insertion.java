3a3480113f16a1f3c0907dc0e6835842848fe807
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-11-13 22:29:52 +0530

    updating sort function!

--- Insertion.java
+++ Insertion.java
@@ -6,3 +6,18 @@
 
 class Insert
 {
+  void insort(int arr[])
+  {
+    for(int i=0;i<arr.length;i++)
+    {
+      int key=arr[i];
+      int j=i-1;
+      while(j>=0 && arr[j]>key)
+      {
+        arr[j+1]=arr[j];
+        j=j-1;
+      }
+      arr[j+1]=key;
+    }
+  }
+  
