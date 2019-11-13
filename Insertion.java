9cc318e56c18f9d8009830e5e4f767d7f28afa61
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-11-13 22:21:16 +0530

    Updating idea of implementation !

---
+++ Insertion.java
@@ -0,0 +1,8 @@
+// here we have an array logically divided into two, thata is sorted and unsorted array.
+// the element in the sorted array is compared with the first element of the unsorted array.
+// if the element is greater, then swapping is done until the correct order and condition is not achieved.
+
+//Code:
+
+class Insert
+{
