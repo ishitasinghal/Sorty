5a43037893890f08e97ad93e949c855584b29d18
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-11-16 08:19:44 +0530

    Function 2 updated !

--- merge.java
+++ merge.java
@@ -31,3 +31,21 @@ public class Mergesort
       }
       k++;
     }
+    while(j<n2)
+    {
+      arr[k]=R[j];
+      j++;
+      k++;
+    }
+  }
+  void sort(int arr[], int l, int r)
+  {
+    if(l<r)
+    {
+      int m=(l+r)/2;
+      sort(arr, l, m);
+      sort(arr, m+1,r);
+      merge(arr, l, m, r);
+    }
+  }
+  
