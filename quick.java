73677d480bfd692a967e6f518002f3a91fea12ea
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-11-17 22:59:49 +0530

    Create quick.java

---
+++ quick.java
@@ -0,0 +1,4 @@
+// this code is to implement quick sort.
+// This works on the divide and conquer approach.
+// One element is taken as a pivot.
+// then the array is traversed, finding the smallest element and comparing the element.
