210961ce8d7021625102470a6d1cfbc615fe0dde
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-11-18 14:12:54 +0530

    Few lines of code

--- quick.java
+++ quick.java
@@ -2,3 +2,18 @@
 // This works on the divide and conquer approach.
 // One element is taken as a pivot.
 // then the array is traversed, finding the smallest element and comparing the element.
+
+//CODE
+
+public class Quick
+{
+  int partition(int arr[], int l, int h) //l for low, h for high
+  {
+    int pivot=arr[h];
+    int i=(l-1);
+    for(int j=l;j<h;j++)
+    {
+      if(arr[j]<pivot)
+      {
+        i++;
+        int temp=arr[i];
